84014867b877eeccd299f6ce63fc22bc2371dff7
Angelos Exarchos <43998058+aexarch@users.noreply.github.com>
2020-01-05 06:51:32 +0200

    Added additional checks and a confirmation before download.
    See changes within code.

--- bash/getubuntu.sh
+++ bash/getubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -e "Ubuntu ISO Downloader for Desktop x64 architectures\n"
-echo -e "This script requires an active internet connection and superuser permissions.\nPlease ensure that you have access to both before proceeding."
+echo -e "This bash script requires an active internet connection and may also require superuser permissions.\nPlease ensure that you have both before proceeding."
 cat < /dev/null > /dev/tcp/8.8.8.8/53; ONLINE=$( echo $? )
 if [ $ONLINE -eq 0 ]; then
     echo -e "\nThe network connection is up. Proceeding...\n"
@@ -8,15 +8,19 @@ else
   echo -e "\nThe network connection is down.\nPlease connect to the internet and try again."
   exit
 fi
-echo -e "\nIn order for this bash script to work you first need to install the wget package for your distro, if not present.\nThis will require superuser permissions.\nAn APT package list update will be necessary too, beforehand."
-echo -e "\nAttempting to update package lists and install the prerequisites through APT...\nPlease provide superuser permissions if needed.\n"
+echo -e "\nIn order for this bash script to work you first need to install the wget package, if not present.\nThis will require superuser permissions.\nAn APT package list update will be necessary too."
 if [[ -z $(which wget) ]]; then
     echo -e "\nwget package is not installed."
     if [[ ! -z $(which apt-get) ]]; then
-        echo -e "Attempting to install via APT. Please provide superuser permissions.\n"
+        echo -e "\nAttempting to update package list and install via APT. Please provide superuser permissions.\n"
         sudo apt-get update
         sudo apt-get install wget
-        echo "\nDone. Proceeding...\n"
+        if [ $? -eq 0 ]; then
+            echo "\nDone. Proceeding...\n"
+        else
+            echo -e "\nThere was an error while installing the missing wget package. apt-get exit code is: $?\nPlease install wget manually and rerun this script."
+            exit
+        fi
     else
         echo -e "\nUnable to retrieve missing package wget.\nYou'll have to manually install it and rerun this script."
         exit
@@ -27,22 +31,38 @@ wget -r --spider -l0 -A iso ftp://releases.ubuntu.com/releases/.pool/ 2>&1 | gre
 readarray urlarr < urls.txt
 cat urls.txt | awk -F"-" '{ print $2 }' > vnrs.txt
 readarray vnrarr < vnrs.txt
+if [ ${#vnrarr[@]} -eq 0 ]; then
+    echo -e "\nThe filelist returned seems to be empty. Please check connectivity and retry later.\nIf this issue persists, please contact the developer of this script.\n"
+    echo -e "Cleaning up and terminating script..."
+    rm -rf releases.ubuntu.com
+    rm urls.txt
+    rm vnrs.txt
+    exit
+fi
 VERSION=""
 while [[ $VERSION = "" ]]; do
     echo -e "\nPlease enter the choice number corresponding to the Ubuntu version you want to download.\n"
     select VERSION in "${vnrarr[@]}"; do
-         if [[ $VERSION = "" ]]; then
-              echo -e "\nInvalid choice! Please enter a number from 1 to ${#vnrarr[@]}.\n"
-         else
-              ARRVNR=$(( REPLY - 1 ))
-              TARGET=$urlarr[$ARRVNR]
-              echo -e "\nFile selected: $TARGET"
-              echo -e "\nInitiating download...\n"
-              wget $TARGET -q -d -c --tries=0 --read-timeout=30 --show-progress --progress=bar:force 2>&1
-              echo -e "\nDone! Thank you for using my script.\n"
-         fi
-         break
-     done
+        if [[ $VERSION = "" ]]; then
+            echo -e "\nInvalid choice! Please enter a number from 1 to ${#vnrarr[@]}.\n"
+        else
+            ARRVNR=$(( REPLY - 1 ))
+            TARGET=$urlarr[$ARRVNR]
+            echo -e "\nFile selected: $TARGET\n"
+            read -p "Download the file? (Type Y to download or anything else to exit this script) " -n 1 -r
+            if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+                echo -e "User aborted. Tidying up and exiting script."
+                rm -rf releases.ubuntu.com
+                rm urls.txt
+                rm vnrs.txt
+                exit
+            fi
+            echo -e "\nInitiating download...\n"
+            wget $TARGET -q -d -c --tries=0 --read-timeout=30 --show-progress --progress=bar:force 2>&1
+            echo -e "\nDone! Thank you for using my script.\n"
+        fi
+        break
+    done
 done
 echo "\nTidying up and exiting script."
 rm -rf releases.ubuntu.com
